2a6abb0550d9878de7915416c2325557b02280fa
MattBroihier <mattbroihier@gmail.com>
2021-01-29 19:23:31 -0600

    add worksheet, works like console app

--- app/TestCode.ws.kts
+++ app/TestCode.ws.kts
@@ -1,4 +1,16 @@
-var i = 1
-i++
-println("Hello World")
-println (i)
+//var i = 1
+//i++
+//println("Hello World")
+//println (i)
+
+val timer = object: CountDownTimer(20000, 1000) {
+    override fun onTick(millisUntilFinished: Long) {
+        println("do something")
+    }
+
+    override fun onFinish() {
+        println("do something else")
+    }
+}
+timer.start()
+//timer.toString()
